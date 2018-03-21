534e4a201aee04083de7e39862957a730be476ca
David <dgawalkiewicz@gmail.com>
2018-03-21 20:02:11 +0100

    fixing bugs

--- scrolltotop.js
+++ scrolltotop.js
@@ -3,26 +3,21 @@ var scrollAnimationValue;
 
 var startScrollAnimation = function(event){
 	event.preventDefault();
-	scrollAnimationValue == window.scrollY;
-    scrollAnimationTimer = setInterval(scrollAnimationStep, 25);
+	scrollAnimationValue = window.scrollY;
+   scrollAnimationTimer = setInterval(scrollAnimationStep, 25);
 };
 
-var scrollAnimationStep = function(){
-		if(window.scrollY != 0) {
-			setTimeout(function() {
-			   window.scrollTo(0,window.scrollY-30);
-				TopscrollTo();
-            }, 100);
-           }
+var scrollAnimationStep = function() {
+  window.scrollTo(0, scrollAnimationValue);
+	scrollAnimationValue -= 100;
 
+	if(scrollAnimationValue <= 0){
+
+		clearInterval(scrollAnimationTimer);
+	}
 };
 
 var scrollToTopButtons = document.getElementsByClassName('button-top');
 for(var i = 0; i < scrollToTopButtons.length; i++){
 	scrollToTopButtons[i].addEventListener('click', startScrollAnimation);
 }
-
-
-if (document.body.scrollTop === 0) {
-    return = false;
-  }
